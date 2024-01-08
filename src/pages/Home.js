d1517d8640bd3077896477750a077cb27dca4c73
joelnyongesa <joelnyongesa148@gmail.com>
2024-01-08 23:19:02 +0300

    Added pages - about, home, reviews and fooer

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='bg-primary text-white h-screen flex items-center justify-center'>
+      <div className='w-full h-4/6 rounded-sm bg-hero bg-cover bg-center'>
+        <p>Home</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
